a430710154548e052702930dc7daee78e7ec8e6e
jakecernet <117542696+jakecernet@users.noreply.github.com>
2023-05-02 09:53:28 +0200

    ui improved but controling not working

--- snake.js
+++ snake.js
@@ -2,6 +2,14 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
+// Get speed slider element and value span
+var speedSlider = document.getElementById("speed");
+var speedValue = document.getElementById("speed-value");
+
+// Get start and pause button elements
+var startButton = document.getElementById("start-button");
+var pauseButton = document.getElementById("pause-button");
+
 // Define the snake object
 var snake = {
     x: 0,
@@ -9,7 +17,8 @@ var snake = {
     dx: 10,
     dy: 0,
     cells: [], // An array that will contain the body of the snake
-    maxCells: 4 // The initial length of the snake
+    maxCells: 4, // The initial length of the snake
+    speed: 5 // The initial speed of the snake
 };
 
 // Define the food object
@@ -38,97 +47,100 @@ function updateSnake() {
     }
 }
 
-// Draw the snake on the canvas
-function drawSnake() {
-    ctx.fillStyle = "green";
-    // Draw the head of the snake
-    ctx.fillRect(snake.x, snake.y, 10, 10);
-    // Loop through the snake's body and draw each cell
-    for (var i = 0; i < snake.cells.length; i++) {
-        ctx.fillRect(snake.cells[i].x, snake.cells[i].y, 10, 10);
+// Check if the snake has collided with the walls or itself
+if (snake.x < 0 || snake.x >= canvas.width || snake.y < 0 || snake.y >= canvas.height) {
+    gameOver();
+}
+for (var i = 1; i < snake.cells.length; i++) {
+    if (snake.cells[i].x === snake.x && snake.cells[i].y === snake.y) {
+        gameOver();
+        break;
     }
 }
 
-// Generate random coordinates for the food
-function generateFood() {
+// Check if the snake has eaten the food
+if (snake.x === food.x && snake.y === food.y) {
+    // Increase the length of the snake
+    snake.maxCells++;
+
+    // Generate new coordinates for the food
     food.x = getRandomCoord();
     food.y = getRandomCoord();
 }
 
-// Draw the food on the canvas
-function drawFood() {
+// Draw the snake and the food
+function draw() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Draw the snake
+    ctx.fillStyle = "green";
+    snake.cells.forEach(function (cell, index) {
+        ctx.fillRect(cell.x, cell.y, 10, 10);
+    });
+
+    // Draw the food
     ctx.fillStyle = "red";
     ctx.fillRect(food.x, food.y, 10, 10);
 }
 
-// Check for collisions between the snake and the food
-function checkFoodCollision() {
-    if (snake.x === food.x && snake.y === food.y) {
-        snake.maxCells++;
-        generateFood();
-    }
+// The main game loop
+function loop() {
+    // Update the position of the snake
+    updateSnake();
+    // Draw the snake and the food
+    draw();
+
+    // Set the game loop to run again after a certain amount of time based on the speed of the snake
+    setTimeout(function () {
+        requestAnimationFrame(loop);
+    }, 1000 / snake.speed);
 }
 
-// Check for collisions between the snake and the walls
-function checkWallCollision() {
-    if (snake.x < 0 || snake.x >= canvas.width || snake.y < 0 || snake.y >= canvas.height) {
-        reset();
-    }
-}
-
-// Check for collisions between the snake and itself
-function checkSelfCollision() {
-    for (var i = 1; i < snake.cells.length; i++) {
-        if (snake.x === snake.cells[i].x && snake.y === snake.cells[i].y) {
-            reset();
-        }
-    }
-}
-
-// Reset the game
-function reset() {
+// Start the game
+function startGame() {
+    // Reset the snake and the food
     snake.x = 0;
     snake.y = 0;
     snake.cells = [];
     snake.maxCells = 4;
+    snake.speed = parseInt(speedSlider.value);
     snake.dx = 10;
     snake.dy = 0;
-    generateFood();
+    food.x = getRandomCoord();
+    food.y = getRandomCoord();
+    // Set up the UI
+    speedValue.textContent = snake.speed;
+    startButton.style.display = "none";
+    pauseButton.style.display = "inline-block";
+
+    // Start the game loop
+    loop();
 }
 
-// Handle keydown events to change the direction of the snake
-document.addEventListener("keydown", function (e) {
-    // Prevent the snake from reversing
-    if (e.which === 37 && snake.dx === 0) {
-        snake.dx = -10;
-        snake.dy = 0;
-    } else if (e.which === 38 && snake.dy === 0) {
-        snake.dy = -10;
-        snake.dx = 0;
-    } else if (e.which === 39 && snake.dx === 0) {
-        snake.dx = 10;
-        snake.dy = 0;
-    } else if (e.which === 40 && snake.dy === 0) {
-        snake.dy = 10;
-        snake.dx = 0;
-    }
+// Pause the game
+function pauseGame() {
+    // Stop the game loop
+    cancelAnimationFrame(loop);
+    // Set up the UI
+    startButton.style.display = "inline-block";
+    pauseButton.style.display = "none";
+}
+
+// End the game
+function gameOver() {
+    // Stop the game loop
+    cancelAnimationFrame(loop);
+    // Set up the UI
+    startButton.style.display = "inline-block";
+    pauseButton.style.display = "none";
+    alert("Game over!");
+}
+
+// Add event listeners for the speed slider and the start and pause buttons
+speedSlider.addEventListener("input", function () {
+    snake.speed = parseInt(speedSlider.value);
+    speedValue.textContent = snake.speed;
 });
 
-// Start the game loop
-function loop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    updateSnake();
-    checkFoodCollision();
-    checkWallCollision();
-    checkSelfCollision();
-    drawSnake();
-    drawFood();
-}
-
-// Generate the first food
-generateFood();
-
-//add speed slider
-var speedSlider = document.getElementById("speed-slider");
-
-var gameLoop = setInterval(loop, speedSlider.value);
+startButton.addEventListener("click", startGame);
+pauseButton.addEventListener("click", pauseGame);
